c35e5fc148f00ba8ca73d2682f96a38883ab582a
Matea Velichkovska <146847415+velichkovskaM@users.noreply.github.com>
2025-05-30 11:09:58 +0200

    Create settings screen; create store

--- app/src/main/java/si/uni_lj/fe/tunv/alarmmeup/ui/components/StoreItemData.kt
+++ app/src/main/java/si/uni_lj/fe/tunv/alarmmeup/ui/components/StoreItemData.kt
@@ -3,6 +3,9 @@ package si.uni_lj.fe.tunv.alarmmeup.ui.data
 data class StoreItemData(
     val id: Int,
     val name: String,
+    val cost: Int,
+    var playing: Boolean = false,
     val imageUrl: String? = null,
-    val description: String = ""
+    val description: String = "",
+    val soundResourceId: Int? = null
 )
